40dfb5c89e24bdac78e4ae6bf7286eab061f7a37
rohit97315 <rohitlamba97315@gmail.com>
2026-02-10 14:32:34 +0530

    worked on landing page frontend

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../App.css";
+import { Link } from 'react-router-dom';
 
 
 export default function LandingPage(){
@@ -17,6 +18,25 @@ export default function LandingPage(){
                     </div>
                 </div>
             </nav>
+
+            <div className="landingMainContainer">
+                <div>
+                    <h1><span style={{color:"#FF9839"}}>Connect </span>with your loved Ones</h1>
+
+
+                    <p>Cover a distance by Video Call</p>
+                    <div role="button">
+                        <Link to={"/home"}>Get Started</Link>
+                    </div>
+                </div>
+                <div>
+                    <img src="/mobile.png" alt=""></img>
+                </div>
+            </div>
+
+
+
+
         </div>
     )
 }
