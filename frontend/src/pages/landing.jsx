18f556a6d09639fbe6c0f1a1f569301cdecd25e2
rohit97315 <rohitlamba97315@gmail.com>
2026-02-10 13:56:00 +0530

    landing page header part made

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import "../App.css";
+
 
 export default function LandingPage(){
     return (
-        <div>LandingPage</div>
+        <div className="landingPageContainer">
+            <nav>
+                <div className="navHeader" >
+                    <h2>Apna Video Call</h2>
+                </div>
+                <div className="navlist">
+                    <p>Join as Guest</p>
+                    <p>Register</p>
+                    <div role="button">
+                        <p>Login</p>
+                    </div>
+                </div>
+            </nav>
+        </div>
     )
 }
