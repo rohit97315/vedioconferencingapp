c5e9ba7436bb101d37f4843b27fa4e3f8016ee10
rohit97315 <rohitlamba97315@gmail.com>
2026-02-10 11:24:30 +0530

    working on app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route, BrowserRouter as Router , Routes } from "react-router-dom";
+import LandingPage from './pages/landing';
+
 
 function App() {
   return (
-    <></>
+    <>
+      <Router>
+        <Routes>
+            <Route path='/' element={<LandingPage/>} />
+        </Routes>
+
+
+
+
+      </Router>
+    </>
   );
 }
 
